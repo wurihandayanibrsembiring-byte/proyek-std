e1139708d29284b9056dbefd03366a040a68f2e2
melianamanullang <melianamanullang817@gmail.com>
2026-06-02 14:21:31 +0700

    searching.cpp

--- searching.cpp
+++ searching.cpp
@@ -2,3 +2,10 @@
 #include <vector>
 #include <cmath>
 using namespace std;
+
+int jumpSearchCore(const vector<int>& arr, int target, int& steps) {
+    int n = arr.size();
+    int step = sqrt(n);
+    int prev = 0;
+    steps = 0;
+    
