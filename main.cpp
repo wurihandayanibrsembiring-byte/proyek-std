660abbd101ea25d83ab73a9e19d9cb69562fe4dd
wurihandayanibrsembiring-byte <wurihandayanibrsembiring@gmail.com>
2026-06-02 14:30:24 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,3 +7,22 @@
 
 #include "sorting.cpp"
 #include "searching.cpp"
+
+using namespace std;
+
+vector<int> dataUtama;
+vector<int> dataTerurut;
+
+void inputManual() {
+    int n;
+    cout << "Masukkan jumlah data: ";
+    cin >> n;
+    dataUtama.clear();
+    for (int i = 0; i < n; i++) {
+        int nilai;
+        cout << "Data ke-" << i + 1 << ": ";
+        cin >> nilai;
+        dataUtama.push_back(nilai);
+    }
+    cout << "=> Data berhasil disimpan secara manual.\n";
+}
