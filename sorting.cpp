91acb67ac1c390f19c8566b97706e8e78d5f7b02
fionahut <fionahutagalung1212@gmail.com>
2026-06-02 14:14:04 +0700

    sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -7,3 +7,20 @@ void cetakArray(const vector<int>& arr) {
     cout << "\n";
 }
 
+void shellSortCore(vector<int>& arr, int& comparisons, int& swaps, bool verbose) {
+    int n = arr.size();
+    comparisons = 0; swaps = 0;
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        if (verbose && n <= 20) cout << "\n-> Jarak (Gap) = " << gap << "\n";
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap; j -= gap) {
+                comparisons++;
+                if (arr[j - gap] > temp) {
+                    arr[j] = arr[j - gap];
+                    swaps++;
+                } else {
+                    break;
+                }
+            }
